ef7fd6828ca113f1abbbc8f6a60acb00bff4bf33
~$ sudo <sudo@onlysq.ru>
2026-08-15 22:50:01 +0300

    fix: enable core library desugaring for android build

--- lib/services/update_checker.dart
+++ lib/services/update_checker.dart
@@ -131,11 +131,11 @@ class UpdateChecker {
       _cachedCurrentVersion = info.version;
       return info.version;
     } catch (_) {
-      return '1.6.0';
+      return '1.6.1';
     }
   }
 
-  static String get currentVersion => _cachedCurrentVersion ?? '1.6.0';
+  static String get currentVersion => _cachedCurrentVersion ?? '1.6.1';
 
   static Future<AppUpdateInfo?> checkForUpdates({String repo = defaultRepo}) async {
     try {
